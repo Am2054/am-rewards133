4e6bad764b263672531a7dabc22e2163936264f3
Am2054 <amirfg992005@gmail.com>
2025-11-10 00:36:53 +0200

    Delete pages directory
    Signed-off-by: Am2054 <amirfg992005@gmail.com>

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ fontFamily: 'Cairo, sans-serif', textAlign: 'center', marginTop: '100px' }}>
-      <h1>✅ Am Rewards يعمل بنجاح!</h1>
-      <p>تم حل كل الأخطاء يا أمير 🎉</p>
-    </div>
-  );
-    }
