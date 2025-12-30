1cf4f43eace4bc5c8ee4582f048f950a6176fee7
Am2054 <amirfg992005@gmail.com>
2025-12-30 10:56:46 +0200

    Update withdraw.js
    Signed-off-by: Am2054 <amirfg992005@gmail.com>

--- api/withdraw.js
+++ api/withdraw.js
@@ -87,7 +87,7 @@ export default async function handler(req, res) {
       tr.set(withdrawalRef, withdrawalData);  
     });  
 
-    return res.status(200).json({ success: true, message: "تم إرسال طلب السحب بنجاح." });
+    return res.status(200).json({ success: true, message: " تم إرسال طلب السحب بنجاح وسيتم مراجعته خلال 24 ساعه." });
 
   } catch (err) {
     const cleanMessage = err.message.includes(':') ? err.message.split(':').pop().trim() : err.message;
