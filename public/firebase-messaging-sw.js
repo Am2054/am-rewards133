508d08e61dcf147b3a9bcf767aaf94a863965a44
Am2054 <amirfg992005@gmail.com>
2026-02-17 05:27:13 +0200

    Update firebase-messaging-sw.js
    Signed-off-by: Am2054 <amirfg992005@gmail.com>

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,6 +3,10 @@ importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-comp
 
 firebase.initializeApp({
     apiKey: "AIzaSyBfOJPkWbmcJ6s29bDNysr-H0Kx-Js3Gy0",
+    authDomain: "am--rewards.firebaseapp.com",
+    databaseURL: "https://am--rewards-default-rtdb.firebaseio.com",
+    projectId: "am--rewards",
+    storageBucket: "am--rewards.firebasestorage.app",
     messagingSenderId: "744783579735",
     appId: "1:744783579735:web:45e00de9998893bbc9b112"
 });
@@ -10,8 +14,11 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-    self.registration.showNotification(payload.notification.title, {
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
         body: payload.notification.body,
-        icon: 'https://cdn-icons-png.flaticon.com/512/633/633600.png'
-    });
+        icon: 'https://cdn-icons-png.flaticon.com/512/633/633600.png',
+        badge: 'https://cdn-icons-png.flaticon.com/512/633/633600.png'
+    };
+    self.registration.showNotification(notificationTitle, notificationOptions);
 });
